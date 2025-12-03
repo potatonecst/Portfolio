ebb596447587def0b85be3f705d8a8396aaba2fa
あぬ <212137539+potatonecst@users.noreply.github.com>
2025-12-03 20:17:52 +0900

    お問い合わせのAPIを更新。

--- app/api/contact/route.ts
+++ app/api/contact/route.ts
@@ -2,13 +2,24 @@ import { NextResponse } from "next/server";
 import { Resend } from "resend";
 import { ContactEmail } from "@/components/emails/contact-email";
 
-const resend = new Resend(process.env.RESEND_API_KEY);
-
-const FROM_EMAIL = process.env.FROM_EMAIL as string; //送信元メールアドレス
-const ADMIN_EMAIL = process.env.ADMIN_EMAIL as string; //管理者メールアドレス
-
 export async function POST(request: Request) {
     try {
+        const apiKey = process.env.RESEND_API_KEY; //APIキー
+        const FROM_EMAIL = process.env.FROM_EMAIL as string; //送信元メールアドレス
+        const ADMIN_EMAIL = process.env.ADMIN_EMAIL as string; //管理者メールアドレス
+
+        if (!apiKey) {
+            console.error("Error: RESEND_API_KEY is missing in environment variables.")
+            return NextResponse.json({ success: false, error: "Server Configuration Error" }, { status: 500 });
+        }
+
+        const resend = new Resend(apiKey);
+
+        if (!FROM_EMAIL || !ADMIN_EMAIL) {
+            console.error("Error: FROM_EMAIL or ADMIN_EMAIL is missing in environment variables.")
+            return NextResponse.json({ success: false, error: "Server Configuration Error" }, { status: 500 });
+        }
+
         const { email, message } = await request.json();
 
         await Promise.all([
